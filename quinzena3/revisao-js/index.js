3127b398543220aaa740488191a4024f163b8e6b
FredNeves95 <fredbneves95@gmail.com>
2021-10-05 20:55:04 -0300

    exercicio 1

---
+++ quinzena3/revisao-js/index.js
@@ -0,0 +1,14 @@
+/*
+Exercícios de fixação:
+Passos para solucionar desafios na programação:
+1- Entender o real problema e o que se quer realizar.
+2- Identificar as informações necessárias para resolução do problema.
+3- Identificar quais são as entradas e quais devem ser as saídas.
+4- Criar um fluxograma da resolução do problema.
+5- Codar, respeitando os itens acima.
+6- Fazer o teste do código.
+7- Analisar o código novamente, para identificar possíveis melhorias.
+
+Esses 7 passos são importantíssimos para resoluções de problemas, pois te permitem ver com mais clareza a resolução.
+*/
+
